c27a10e89f5964fbfd87c1ef6008d367e551b5c6
vikasazad <vikumar.azad@gmail.com>
2025-03-21 10:29:58 +0530

    21/03/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Main from "./modules/main/components/main";
 
 export default async function Home() {
   const data: any = await getHotelData("8851280284");
+  console.log('Data', data)
   return (
     <div className="p-2">
       <Header data={data.info} />
